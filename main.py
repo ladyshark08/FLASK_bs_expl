5a3929c35229f31e4660cdfa653ba8816837f793
ladyshark08 <150834352+ladyshark08@users.noreply.github.com>
2024-01-31 15:05:32 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from flask import Flask, render_template, url_for
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+API_URL = "https://api.npoint.io/fec9dce85d355bdc9d52"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/')
+def home():
+    response = requests.get(API_URL).json()
+    return render_template("index.html", blog_posts=response)
+    # return render_template("index.html")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route("/post/<int:num>")
+def get_blog(num):
+    response = requests.get(API_URL).json()[num - 1]
+    return render_template("post.html", message=response)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+app.run(debug=True)
