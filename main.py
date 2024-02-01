002ddf3e7e8c3d5429148285b190374f28829a5c
ladyshark08 <150834352+ladyshark08@users.noreply.github.com>
2024-02-01 10:17:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import requests
+import smtplib
+import os
 
 app = Flask(__name__)
 
@@ -27,6 +29,23 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+
+@app.route('/form_entry', methods=['post'])
+def receive_data():
+    my_email = "m43217438@gmail.com"
+    password = os.environ.get("PASSWORD")
+    email = request.form.get("email")
+    name = request.form.get("name")
+    phone = request.form.get("phone")
+    message = request.form.get("message")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as new_connect:
+        new_connect.starttls()
+        new_connect.login(user=my_email, password=password)
+        new_connect.sendmail(from_addr=my_email, to_addrs=my_email,
+                             msg=f"Subject:New Message\n\nHello\n\nName:{name}\nPhone:{phone}\nEMail:{email}\nMessage:{message}")
+
+    return render_template("success.html")
 
 
 app.run(debug=True)
